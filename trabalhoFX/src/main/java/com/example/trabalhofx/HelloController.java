9f62d7c694f527a4ddc91b6165e66034182d360f
Joao-Paulo-Bortolocce <bbortolocce@gmail.com>
2025-03-15 17:28:41 -0300

    adicionando principal

--- trabalhoFX/src/main/java/com/example/trabalhofx/HelloController.java
+++ trabalhoFX/src/main/java/com/example/trabalhofx/HelloController.java
@@ -1,14 +1,27 @@
 package com.example.trabalhofx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+
+    public void onIniciar(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Stage stage = new Stage();
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ trabalhoFX/src/main/java/com/example/trabalhofx/MenuController.java
@@ -0,0 +1,4 @@
+package com.example.trabalhofx;
+
+public class MenuController {
+}
